d4ace0dd84a86fcceb38d7487bbf2caad74b2017
lennon0829 <348221552@qq.com>
2017-08-15 13:52:20 +0800

    lennon
    生产者地址设置为可配置

--- microservice-simple-consumer-movie/src/main/java/com/qdynasty/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/qdynasty/controller/MovieController.java
@@ -4,6 +4,7 @@
 package com.qdynasty.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,17 @@ import com.qdynasty.entity.User;
  */
 @RestController
 public class MovieController {
+	
+	@Value("${user.userServiceUrl}")
+	private String userServiceUrl;
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@GetMapping("/user/{id}")
 	public User findByid(@PathVariable Long id) {
-		return restTemplate.getForObject("http://127.0.0.1:8000/" + id, User.class);
+		System.out.println("user.userServiceUrl=" + userServiceUrl);
+		
+		return restTemplate.getForObject(userServiceUrl + id, User.class);
 	}
 }
