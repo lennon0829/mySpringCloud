e6a1cf374fb3e7fed074f4df58829d5ea0085cfc
lennon0829 <348221552@qq.com>
2017-08-16 18:48:02 +0800

    元数据

--- microservice-consumer-movie/src/main/java/com/qdynasty/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/qdynasty/controller/MovieController.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
