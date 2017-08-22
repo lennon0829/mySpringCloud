36c80f7f1659d50effc742bad9486dbbef8c03a2
lennon0829 <348221552@qq.com>
2017-08-22 14:19:38 +0800

    整合RabbitMQ

---
+++ microservice-hystrix-turbine-mq/src/main/java/com/qdynasty/MicroserviceHystrixTurbineApplication.java
@@ -0,0 +1,16 @@
+package com.qdynasty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@SpringBootApplication
+@EnableTurbineStream
+@EnableDiscoveryClient
+public class MicroserviceHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHystrixTurbineApplication.class, args);
+	}
+}
