bfc1684db6fea2c1e4689e655ace1fe5e71d4fb1
lennon0829 <348221552@qq.com>
2017-09-11 14:34:59 +0800

    加解密和手动刷新

--- microservice-config-client/src/main/java/com/qdynasty/controller/ConfigClientController.java
+++ microservice-config-client/src/main/java/com/qdynasty/controller/ConfigClientController.java
@@ -4,6 +4,7 @@
 package com.qdynasty.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
 	//获取Git仓库配置文件中的profile的值
